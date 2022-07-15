69cae4df40ac2d352b4c96c8a6d133084abbf205
Alexey Turdakov <alexeyturdakov@gmail.con>
2022-07-15 17:35:09 +0400

    task 2

--- js/index.js
+++ js/index.js
@@ -25,5 +25,58 @@ function findIndexSum(val, arr) {
 }
 
 const arr = [2, 5, 8, 9, 22, 57, 94, 100, 127, 198, 345, 451];
-console.log(findIndexSum(79, arr)); // 4 + 5 -> 9
-console.log(findIndexSum(70, arr)); // -1
+// console.log(findIndexSum(79, arr)); // 4 + 5 -> 9
+// console.log(findIndexSum(70, arr)); // -1
+
+/**
+ * Прислал Дмитрий Кутыршин
+ *
+ * Returns the rectangle object with width and height parameters and
+ * getArea() method.
+ *
+ * @param {number} width
+ * @param {number} height
+ * @return {Object}
+ */
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+  this.getArea = function () {
+    return this.width * this.height;
+  };
+}
+
+const r = new Rectangle(10, 20);
+// console.log(r.width); // 10
+// console.log(r.height); // 20
+// console.log(r.getArea()); // 200
+
+/*
+Прислал Dariel. Задача с реального собеседования.
+
+В функцию sumNumbers передается массив, содержащий все подряд (любые
+типы данных). Необходимо реализовать функцию так, чтобы она вернула
+среднее арифметическое всех элементов, которые могут быть нативно
+представлены в javascript в виде числа, т.е. мы считаем значение, если
+при прверащении в число, значение не является NaN.
+*/
+function sumNumbers(arr) {
+  
+}
+
+console.log(
+  sumNumbers([
+    1,
+    "9",
+    NaN,
+    9.5,
+    true,
+    "WebInterview",
+    Symbol("5"),
+    null,
+    // 5n, // В jsfiddle bigint нормально не работает
+    undefined,
+    { a: 5 },
+    () => 100,
+  ])
+); // 4.25

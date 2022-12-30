63b22399f637de53c6346b8ce110362ee7604a37
Alexey Turdakov <alexeyturdakov@gmail.con>
2022-12-30 17:49:47 +0400

    1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
+const person = {
+  name: "Vlad",
+  age: 12,
+  email: "fg@ru",
+};
 
+const op = new Proxy(person, {
+  get(target, prop) {
+    return target[prop];
+  },
+
+  set(target, props, value) {},
+
+  has(target, prop) {},
+});
